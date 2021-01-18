6ba372b5035a1a2864047aa439d02ef71d9b05b6
cpadua912 <cristian.padua912@gmail.com>
2021-01-18 01:02:40 -0500

    Added frontend code

---
+++ client/index.js
@@ -0,0 +1,103 @@
+import Web3 from "web3";
+import EtherWallet from "../build/contracts/EtherWallet.json";
+
+let web3;
+let etherWallet;
+
+const initWeb3 = () => {
+  return new Promise((resolve, reject) => {
+    if (typeof window.ethereum !== "undefined") {
+      web3 = new Web3(window.ethereum);
+      window.ethereum
+        .enable()
+        .then(() => {
+          resolve(new Web3(window.ethereum));
+        })
+        .catch((e) => {
+          reject(e);
+        });
+      return;
+    }
+    if (typeof window.web3 !== "undefined") {
+      return resolve(new Web3(window.web3.currentProvider));
+    }
+    resolve(new Web3("http://localhost:9545"));
+  });
+};
+
+const initContract = async () => {
+  // We can receive address from user of a different network
+  const networkId = await web3.eth.net.getId();
+  return new web3.eth.Contract(
+    EtherWallet.abi,
+    EtherWallet.networks[networkId].address
+  );
+};
+
+const initApp = () => {
+  const $deposit = document.getElementById("deposit");
+  const $depositResult = document.getElementById("deposit-result");
+  const $send = document.getElementById("send");
+  const $sendResult = document.getElementById("send-result");
+  const $balance = document.getElementById("balance");
+  let accounts = [];
+
+  web3.eth.getAccounts().then((_accounts) => {
+    accounts = _accounts;
+  });
+
+  const getBalance = () => {
+    etherWallet.methods
+      .balanceOf()
+      .call()
+      .then((result) => {
+        $balance.innerHTML = result;
+      });
+  };
+  getBalance();
+
+  $deposit.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const amount = e.target.elements[0].value;
+    etherWallet.methods
+      .deposit()
+      .send({ from: accounts[0], value: amount })
+      .then(() => {
+        $depositResult.innerHTML = `${amount} wei has been deposited into this wallet`;
+        getBalance();
+      })
+      .catch((err) => {
+        $depositResult.innerHTML = `Sorry, we are not able to deposit your ether :(`;
+      });
+  });
+
+  $send.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const to = e.target.elements[0].value;
+    const amount = e.target.elements[1].value;
+    etherWallet.methods
+      .send(to, amount)
+      .send({ from: accounts[0] })
+      .then(() => {
+        $sendResult.innerHTML = `${amount} wei has been sent to ${to}`;
+        getBalance();
+      })
+      .catch((err) => {
+        $sendResult.innerHTML = `Sorry, we are not able to send this ether :(`;
+      });
+  });
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  initWeb3()
+    .then((_web3) => {
+      web3 = _web3;
+      return initContract();
+    })
+    .then((_etherWallet) => {
+      etherWallet = _etherWallet;
+      initApp();
+    })
+
+    .catch((e) => console.log(e.message));
+});
---
+++ test/etherWallet.js
@@ -0,0 +1,53 @@
+const EtherWallet = artifacts.require("EtherWallet");
+
+contract("EtherWallet", (accounts) => {
+  let etherWallet = null;
+  before(async () => {
+    etherWallet = await EtherWallet.deployed();
+  });
+
+  it("should set accounts[0] as owner", async () => {
+    const owner = await etherWallet.owner();
+    assert(owner === accounts[0]);
+  });
+
+  it("should deposit ether to wallet", async () => {
+    await etherWallet.deposit({
+      from: accounts[0],
+      value: 100,
+    });
+
+    const balance = await web3.eth.getBalance(etherWallet.address);
+    assert(parseInt(balance) === 100);
+  });
+
+  it("should return the balance of the wallet", async () => {
+    const balance = await etherWallet.balanceOf();
+    assert(parseInt(balance) === 100);
+  });
+
+  it("send ether to another address", async () => {
+    const balanceRecipientBefore = await web3.eth.getBalance(accounts[1]);
+
+    await etherWallet.send(accounts[1], 50, {
+      from: accounts[0],
+    });
+    const balanceWallet = await web3.eth.getBalance(etherWallet.address);
+    assert(parseInt(balanceWallet) === 50);
+
+    const balanceRecipientAfter = await web3.eth.getBalance(accounts[1]);
+    const initialBalance = web3.utils.toBN(balanceRecipientBefore);
+    const finalBalance = web3.utils.toBN(balanceRecipientAfter);
+    assert(finalBalance.sub(initialBalance).toNumber() === 50);
+  });
+
+  it("should NOT transfer ether if transaction is sent from someone other than owner", async () => {
+    try {
+      await etherWallet.send(accounts[1], 50, { from: accounts[2] });
+    } catch (err) {
+      assert(err.message.includes("sender is not allowed"));
+      return;
+    }
+    assert(false);
+  });
+});
